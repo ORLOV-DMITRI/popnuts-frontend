5884ebe8c22ac8ecf1167c500008780a9b9b8904
Orlov Dmitri <dmitri.orlov.anatolevich@gmail.com>
2024-08-11 15:56:17 +0600

    fix middleware bugs

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,11 +5,11 @@ import type { NextRequest } from 'next/server'
 export function middleware(request: NextRequest) {
     const accessToken = request.cookies.get('token')?.value;
     
-    if (request.nextUrl.pathname.startsWith('/lk') && !accessToken) {
+    if (request.nextUrl.pathname.startsWith('/lk/') && !accessToken) {
         return NextResponse.redirect(new URL("/login", request.url));
     }
     if (request.nextUrl.pathname.startsWith('/login') && accessToken) {
-        return NextResponse.redirect(new URL("/lk", request.url));
+        return NextResponse.redirect(new URL("/", request.url));
     }
     return NextResponse.next();
 }
